4504051a817522fd82a28511aa680a48bd466c26
onurkurtulmus <onurk.dev@outlook.com>
2021-10-24 10:05:44 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Loginscreen
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string user;
+            string pass;
+
+            do {
+                Console.WriteLine("Lütfen giriş bilgilerinizi giriniz");
+                Console.Write("Kullanıcı Adı: ");
+                user = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Şifre: ");
+                pass = Console.ReadLine();
+
+
+            } while (user != "admin" && pass != "123");
+
+            Console.WriteLine("Giriş Başarılı \n Devam etmek için bir tuşa basınız.");
+            Console.ReadKey();
+
+          
+
+        }
+    }
+}
